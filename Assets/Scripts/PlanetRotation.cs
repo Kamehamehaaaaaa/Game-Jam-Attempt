c32282f6db838397dd6b1b477f240c2ea2c211fd
Kamehamehaaaaaa <sushantsamani@gmail.com>
2023-12-11 03:12:38 +0530

    first feature commit

---
+++ Assets/Scripts/PlanetRotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetRotation : MonoBehaviour
+{
+    [SerializeField] float rotationSpeed = 25f;
+
+    void Update()
+    {
+        // transform.Rotate(0, 0, Mathf.Abs(Input.GetAxis("Horizontal")) * rotationSpeed * Time.deltaTime, Space.Self);
+        transform.Rotate(0, 0, rotationSpeed * Time.deltaTime, Space.Self);
+    }
+}
---
+++ Assets/Scripts/SpaceshipMovement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipMovement : MonoBehaviour
+{
+    [SerializeField] float rotationSpeed = 5f;
+    [SerializeField] GameObject pivotObject;
+
+    void Update()
+    {
+        transform.RotateAround(pivotObject.transform.position, new Vector3(0, 0, -1), Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime);
+    }
+}
